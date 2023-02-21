85cbfccb671520a48edeaae5d0cb85e252151240
kylechui <kyle.chui+github@pm.me>
2023-02-20 16:19:40 -0800

    refactor: Add type annotations, rewrite logic.

--- lua/virtualtypes.lua
+++ lua/virtualtypes.lua
@@ -1,13 +1,33 @@
-local api = vim.api
-local lsp = vim.lsp
+-- Type annotation for the response of a LSP textDocument/codeLens request, more information can be found here:
+-- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_codeLens
+---@class position
+---@field character integer
+---@field line integer
 
-local M = {}
+---@class range
+---@field start position
+---@field end position
+
+---@class command
+---@field command string
+---@field title string
+
+---@class code_lens
+---@field command command
+---@field range range
+
+---@alias result code_lens[]
+
+---@class response
+---@field result result?
 
 -- Namespace for virtual text messages
-local virtual_types_ns = api.nvim_create_namespace("virtual_types")
+local virtual_types_ns = vim.api.nvim_create_namespace("virtual_types")
 
 -- Plugin status
 local is_enabled = true
+
+local M = {}
 
 -- Clears the buffer of virtual text.
 M.clear_namespace = function()
@@ -15,23 +35,21 @@ M.clear_namespace = function()
 end
 
 -- Writes virtual text to the current buffer.
----@param start_line integer The line number where the message should start.
-M.set_virtual_text = function(start_line, msg)
-  if type(msg[1]) ~= "string" or msg[2] ~= "TypeAnnot" then
-    return
-  end
-  api.nvim_buf_set_extmark(0, virtual_types_ns, start_line, 1, {
-    virt_text = { msg },
+---@param start_line integer The line number where the annotation should be written.
+---@param annotation string The contents of the annotation.
+M.set_virtual_text = function(start_line, annotation)
+  vim.api.nvim_buf_set_extmark(0, virtual_types_ns, start_line, 1, {
+    virt_text = { { annotation, "TypeAnnot" } },
     hl_mode = "combine",
   })
 end
 
-function M.enable()
+M.enable = function()
   is_enabled = true
   M.annotate_types_async()
 end
 
-function M.disable()
+M.disable = function()
   M.clear_namespace()
   is_enabled = false
 end
@@ -40,31 +58,23 @@ local annotate_types = function()
   if is_enabled == false or vim.fn.getcmdwintype() == ":" or #vim.lsp.get_active_clients() == 0 then
     return
   end
+  local parameter = vim.lsp.util.make_position_params(0, nil) ---@diagnostic disable-line
+  local responses = vim.lsp.buf_request_sync(0, "textDocument/codeLens", parameter)
+  ---@cast responses response[]
 
-  local parameter = lsp.util.make_position_params(0, nil)
-  local response = lsp.buf_request_sync(0, "textDocument/codeLens", parameter)
+  vim.pretty_print(responses)
 
   M.clear_namespace()
-
-  if response then
-    for _, v in pairs(response) do
-      if v == nil or v["result"] == nil then
+  if responses then
+    for _, response in pairs(responses) do
+      if response == nil or response.result == nil then
         return
-      end -- no response
-      for _, vv in pairs(v["result"]) do
-        if vv["range"] and vv["command"] then
-          local start_line = -1
-          for _, vvv in pairs(vv["range"]) do
-            start_line = tonumber(vvv["line"])
-          end
-          for _, vvv in pairs(vv["command"]) do
-            if vvv == nil or vvv == "" then
-              goto skip_to_next
-            end
-            local msg = { vvv, "TypeAnnot" }
-            M.set_virtual_text(start_line, msg)
-            ::skip_to_next::
-          end
+      end
+      for _, code_lens in pairs(response.result) do
+        if code_lens.range and code_lens.command then
+          local start_line = code_lens.range["end"].line
+          local annotation = code_lens.command.title
+          M.set_virtual_text(start_line, annotation)
         end
       end
     end
@@ -87,12 +97,13 @@ function M.on_attach(client, _)
   -- Don't use schedule. It somewhat slower on startup.
   annotate_types()
 
-  -- Setup autocmd
+  -- Setup autocommand to refresh the type annotations
   local virtual_types_augroup = vim.api.nvim_create_augroup("virtual_types_refresh", {})
   vim.api.nvim_create_autocmd({
     "BufEnter",
     "BufWinEnter",
     "BufWrite",
+    "CursorMoved",
     "InsertLeave",
     "TabEnter",
   }, {
